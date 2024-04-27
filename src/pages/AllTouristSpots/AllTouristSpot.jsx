199863548e832d37de1ba71a4047b21c0b0d26c0
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-04-27 15:58:58 +0600

    dropdown and info updated in alltourist spots

--- src/pages/AllTouristSpots/AllTouristSpot.jsx
+++ src/pages/AllTouristSpots/AllTouristSpot.jsx
@@ -6,26 +6,33 @@ const AllTouristSpot = ({ spot }) => {
     const { _id, names, photo, country, location, seasonality, cost, time, visitors, name, email, description } = spot;
     return (
 
-        <div className="card w-full bg-base-100 shadow-xl">
-            <figure><img className="lg:h-60 lg:w-full" src={photo} alt="spots" /></figure>
-            <div className="flex justify-between bg-cyan-500  text-white p-2">
-                <div className="flex items-center gap-2">
-                    <ImLocation2></ImLocation2>
-                    <p>{location}, {country}</p>
-                </div>
-                <div className="flex items-center gap-2">
-                    <SlCalender></SlCalender>
-                    <p>{time}</p>
-                </div>
-            </div>
-            <div className="card-body">
-                <h2 className="card-title font-bold">
-                    {names}
+        <div>
 
-                </h2>
+            <div className="card w-full bg-base-100 shadow-xl">
+                <figure><img className="lg:h-60 lg:w-full" src={photo} alt="spots" /></figure>
+                <div className="flex justify-between bg-cyan-500  text-white p-2">
+                    <div className="flex items-center gap-2">
+                        <ImLocation2></ImLocation2>
+                        <p>{location}, {country}</p>
+                    </div>
+                    <div className="flex items-center gap-2">
+                        <SlCalender></SlCalender>
+                        <p>{time}</p>
+                    </div>
+                </div>
+                <div className="card-body">
+                    <div className="flex justify-between">
+                        <h2 className="card-title font-bold">
+                            {names}
+                        </h2>
+                        <div className="font-semibold">{cost}/Person</div>
+                    </div>
 
-                <div className="card-actions justify-between">
-                    <div className="font-semibold">{cost}/Person</div>
+                    <div className="card-actions justify-between">
+                        <div ><span className="font-semibold">Visitors Per Year:</span> {visitors}/Person</div>
+                        <div ><span className="font-semibold">Seasonality:</span> {seasonality}</div>
+
+                    </div>
                     <Link to={`/spots/${_id}`}><div className="badge  text-cyan-500 badge-outline">View Details</div></Link>
                 </div>
             </div>
--- src/pages/AllTouristSpots/AllTouristSpots.jsx
+++ src/pages/AllTouristSpots/AllTouristSpots.jsx
@@ -1,11 +1,21 @@
 import { useLoaderData } from "react-router-dom";
 import AllTouristSpot from "./AllTouristSpot";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 
 const AllTouristSpots = () => {
     const loadedSpots = useLoaderData();
     return (
         <div>
+            <div className="text-center mt-8">
+                <div className="dropdown dropdown-bottom b">
+                    <div tabIndex={0} role="button" className="btn m-1 bg-cyan-500 text-white"><IoMdArrowDropdown></IoMdArrowDropdown>Sort By</div>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <li className="text-cyan-700 font-semibold"><a>Average Cost</a></li>
+
+                    </ul>
+                </div>
+            </div>
             <div className="grid mt-8 md:mt-12 gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-6xl mx-auto">
                 {
                     loadedSpots.map(spot => <AllTouristSpot
