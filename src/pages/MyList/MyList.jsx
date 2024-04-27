1f01a9d8f837243560aa10e09074c4151ff09be3
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-04-28 02:08:24 +0600

    my list UI added

--- src/pages/MyList/MyList.jsx
+++ src/pages/MyList/MyList.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../provider/AuthProvider";
+import { MdEdit } from "react-icons/md";
 
 
 const MyList = () => {
@@ -12,12 +13,45 @@ const MyList = () => {
 
 
     return (
-        <div>
-            {
-                spots.map(spot =>
-                    <div
-                        key={spot.id}>{spot.email}, {spot.names}</div>)
-            }
+        <div >
+
+            <div className="overflow-x-auto px-5">
+                <table className="table bg-cyan-50 mt-8">
+                    {/* head */}
+                    <thead>
+                        <tr className="font-bold text-black text-lg">
+                            <th ></th>
+                            <th >Tourist Spot Name </th>
+                            <th >Location </th>
+                            <th >Country </th>
+                            <th >Average Cost </th>
+                            <th ></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            spots.map(spot =>
+                                user.email === spot.email &&
+                                <tr key={spot._id}>
+                                    <th>1</th>
+                                    <td>{spot.names} </td>
+                                    <td>{spot.location} </td>
+                                    <td>{spot.country}</td>
+                                    <td>{spot.cost}</td>
+                                    <td>
+                                        <button className="btn btn-info"><MdEdit></MdEdit>Update</button>
+                                    </td>
+                                    <td>
+                                        <button className="btn btn-error">X  Delete</button>
+                                    </td>
+                                </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
