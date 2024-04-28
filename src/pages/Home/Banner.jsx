bfecb29a0859c1e5003a7e10182c13c60b287af5
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-04-28 19:43:13 +0600

    dark mode added

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,14 +1,17 @@
-
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import 'swiper/swiper-bundle.css'
-
-
+import 'swiper/swiper-bundle.css';
+import { Typewriter } from 'react-simple-typewriter';
 
 const Banner = () => {
-    return (
+    const slides = [
+        { backgroundImage: 'url(https://i.ibb.co/M5PPzmN/backpacker-standing-sunrise-viewpoint-ja-bo-village-mae-hong-son-province-thailand.jpg)' },
+        { backgroundImage: 'url(https://i.ibb.co/ydpGCgJ/andrew-coelho-m6t-Aq-Zvy4-RM-unsplash.jpg)' },
+        { backgroundImage: 'url(https://i.ibb.co/QMksV3W/amjad-rana-Mr-KPi-yaj-C0-unsplash.jpg)' },
+        { backgroundImage: 'url(https://i.ibb.co/K5dJR80/s-m-ibrahim-1-NEJki-TTu-LU-unsplash.jpg)' }
+    ];
 
+    return (
         <Swiper
             modules={[Navigation, Pagination, Scrollbar, A11y]}
             spaceBetween={50}
@@ -18,73 +21,37 @@ const Banner = () => {
             scrollbar={{ draggable: true }}
             className='z-0'
         >
-            <SwiperSlide>
-                <div className="hero rounded-lg min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/M5PPzmN/backpacker-standing-sunrise-viewpoint-ja-bo-village-mae-hong-son-province-thailand.jpg)' }}>
-                    <div className="hero-overlay rounded-lg bg-opacity-60"></div>
-                    <div className="hero-content text-center text-neutral-content">
-                        <div className="max-w-lg">
-                            <h1 className="mb-5 text-5xl font-bold animate__animated animate__shakeX">Discover the Worlds Wonders with Travel Trails</h1>
-                            <p className="mb-5">Explore the vast diversity of cultures, landscapes, and experiences the world has to offer. From bustling cities to serene natural wonders, there is something for every traveler to discover.</p>
-                            <div className='space-x-5'>
-                                <button className="btn bg-cyan-600 text-white font-semibold">All Spots</button>
-                                <button className="btn bg-transparent text-white font-semibold">Explore</button>
+            {slides.map((slide, index) => (
+                <SwiperSlide key={index}>
+                    <div className="hero rounded-lg min-h-screen" style={{ backgroundImage: slide.backgroundImage }}>
+                        <div className="hero-overlay rounded-lg bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-2xl">
+                                <h1 className='text-3xl md:text-5xl font-semibold'>
+                                    <Typewriter
+                                        words={['Discover the Worlds Wonders with Travel Trails']}
+                                        loop={false}
+                                        cursor
+                                        cursorStyle='_'
+                                        typeSpeed={70}
+                                        deleteSpeed={50}
+                                        delaySpeed={2000}
+                                        onType={(text) => (
+                                            <h1 className="mb-5 text-5xl font-bold animate__animated animate__shakeX" style={{ fontSize: '3rem' }}>{text}</h1>
+                                        )}
+                                    />
+                                </h1>
+                                <p className="mb-5">Explore the vast diversity of cultures, landscapes, and experiences the world has to offer. From bustling cities to serene natural wonders, there is something for every traveler to discover.</p>
+                                <div className='space-x-5'>
+                                    <button className="btn bg-cyan-600 text-white font-semibold">All Spots</button>
+                                    <button className="btn bg-transparent text-white font-semibold">Explore</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className="hero rounded-lg min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/ydpGCgJ/andrew-coelho-m6t-Aq-Zvy4-RM-unsplash.jpg)' }}>
-                    <div className="hero-overlay rounded-lg bg-opacity-60"></div>
-                    <div className="hero-content text-center text-neutral-content">
-                        <div className="max-w-lg">
-                            <h1 className="mb-5 text-5xl font-bold animate__animated animate__shakeX">Discover the Worlds Wonders with Travel Trails</h1>
-                            <p className="mb-5">Explore the vast diversity of cultures, landscapes, and experiences the world has to offer. From bustling cities to serene natural wonders, there is something for every traveler to discover.</p>
-                            <div className='space-x-5'>
-                                <button className="btn bg-cyan-600 text-white font-semibold">All Spots</button>
-                                <button className="btn bg-transparent text-white font-semibold">Explore</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className="hero rounded-lg min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/QMksV3W/amjad-rana-Mr-KPi-yaj-C0-unsplash.jpg)' }}>
-                    <div className="hero-overlay rounded-lg bg-opacity-60"></div>
-                    <div className="hero-content text-center text-neutral-content">
-                        <div className="max-w-lg">
-                            <h1 className="mb-5 text-5xl font-bold animate__animated animate__shakeX">Discover the Worlds Wonders with Travel Trails</h1>
-                            <p className="mb-5">Explore the vast diversity of cultures, landscapes, and experiences the world has to offer. From bustling cities to serene natural wonders, there is something for every traveler to discover.</p>
-                            <div className='space-x-5'>
-                                <button className="btn bg-cyan-600 text-white font-semibold">All Spots</button>
-                                <button className="btn bg-transparent text-white font-semibold">Explore</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </SwiperSlide>
-            <SwiperSlide>
-                <div className="hero rounded-lg min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/K5dJR80/s-m-ibrahim-1-NEJki-TTu-LU-unsplash.jpg)' }}>
-                    <div className="hero-overlay rounded-lg bg-opacity-60"></div>
-                    <div className="hero-content text-center text-neutral-content">
-                        <div className="max-w-lg">
-                            <h1 className="mb-5 text-5xl font-bold animate__animated animate__shakeX">Discover the Worlds Wonders with Travel Trails</h1>
-                            <p className="mb-5">Explore the vast diversity of cultures, landscapes, and experiences the world has to offer. From bustling cities to serene natural wonders, there is something for every traveler to discover.</p>
-                            <div className='space-x-5'>
-                                <button className="btn bg-cyan-600 text-white font-semibold">All Spots</button>
-                                <button className="btn bg-transparent text-white font-semibold">Explore</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </SwiperSlide>
-            {/* Add more slides as needed */}
+                </SwiperSlide>
+            ))}
         </Swiper>
-
     );
 };
 
